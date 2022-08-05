1a53cb83046c08cf6a45877ddb8d2d5b4fa916f6
zahra <90015662+zsarker4@users.noreply.github.com>
2022-08-04 17:54:30 -0700

    Update ReservationReader.h

--- ReservationReader.h
+++ ReservationReader.h
@@ -1,14 +1,3 @@
-/*
-	Hello World
-
-	Tran, Kim Thien
-	Sarker, Zahradinee
-
-	Spring 2022
-	CS A250 - C++ 2
-	Project: Hiking in the US
-*/
-
 #ifndef RESERVATIONREADER_H
 #define RESERVATIONREADER_H
 
